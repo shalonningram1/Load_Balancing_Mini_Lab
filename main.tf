f5c314d0ee5a145d1dc27b7c668c54bfc783943d
shalonningram1 <mscianika@gmail.com>
2023-03-03 15:16:32 -0500

    updating files

--- main.tf
+++ main.tf
@@ -25,3 +25,45 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_route_table" "aws_internet_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "internet-route-table"
+  }
+}
+
+resource "aws_route_table_association" "route_tbl_assoc_public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.aws_internet_route_table.id
+}
+
+
+ resource "aws_security_group" "sg" {
+  name        = "dev_sg"
+  description = "dev security group"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "open-sg"
+  }
+}
